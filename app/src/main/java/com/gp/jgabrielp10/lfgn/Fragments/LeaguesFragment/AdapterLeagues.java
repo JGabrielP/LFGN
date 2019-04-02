d76f25fb9813d0295dd3317af0632ecc8bd9d4c3
JGABRIELP10 <jgabrielpm.10@gmail.com>
2019-04-02 12:55:28 -0700

    Changes in Leaderboard and Leadergoal modules

--- app/src/main/java/com/gp/jgabrielp10/lfgn/Fragments/LeaguesFragment/AdapterLeagues.java
+++ app/src/main/java/com/gp/jgabrielp10/lfgn/Fragments/LeaguesFragment/AdapterLeagues.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.gp.jgabrielp10.lfgn.Models.League;
 import com.gp.jgabrielp10.lfgn.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -33,8 +34,8 @@ class AdapterLeagues extends RecyclerView.Adapter<AdapterLeagues.mViewHolder> {
 
         public void bind(final League item, final OnItemClickListener listener) {
             textView.setText(item.Name);
-            //Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/lfgn-web.appspot.com/o/ligafgn%40gmail.com%2FuserImage%2FawRtO6xTwJQTU99MIlg0sNqEy5K2?alt=media&token=a21f7faa-f469-496b-9830-5043eadda928").placeholder(R.drawable.no_logo).into(logo);
-            logo.setImageResource(R.drawable.no_logo);
+            Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/lfgn-web.appspot.com/o/ligafgn%40gmail.com%2FuserImage%2FawRtO6xTwJQTU99MIlg0sNqEy5K2?alt=media&token=a21f7faa-f469-496b-9830-5043eadda928").placeholder(R.drawable.no_logo).into(logo);
+            //logo.setImageResource(R.drawable.no_logo);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
